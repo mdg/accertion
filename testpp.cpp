3d9a8c0362fed88942c8ce0b5792053768ae1798
Matthew Graham <mdg149@optonline.net>
2008-10-04 10:06:27 -0400

    added a feature to call an individual test from the command line

--- testpp.cpp
+++ testpp.cpp
@@ -85,6 +85,8 @@ bool testpp_runner_i::in_suite( const std::string &suite_name ) const
 {
 	if ( suite_name == m_file_name ) {
 		return true;
+	} else if ( suite_name == m_test_name ) {
+		return true;
 	} else if ( m_suite && m_suite->match( suite_name ) ) {
 		return true;
 	} else if ( ( suite_name +".cpp" ) == m_file_name ) {
