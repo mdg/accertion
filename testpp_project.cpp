fa1ab367d94d8bbe6bac503cbf0d805b5b2ae412
Matthew Graham <mdg149@optonline.net>
2008-10-04 20:27:09 -0400

    added the initial version of the testpp_project class

---
+++ testpp_project.cpp
@@ -0,0 +1,41 @@
+#include "testpp_project.h"
+
+
+/**
+ * The default project loader class.
+ */
+class default_project_loader_c
+: public testpp_project_loader_i
+{
+public:
+	virtual testpp_project_c * create_project()
+	{
+		return new testpp_project_c();
+	}
+};
+
+
+testpp_project_c::testpp_project_c()
+{
+	add_output< human_testpp_output_c >();
+	add_output< yaml_testpp_output_c >();
+	add_output< json_testpp_output_c >();
+}
+
+testpp_project_c::~testpp_project_c()
+{
+	std::list< testpp_output_i * >::iterator output;
+	for ( output=m_output.begin(); output!=m_output.end(); ++output ) {
+		delete *it;
+	}
+	m_output.clear();
+}
+
+
+testpp_project_loader_i * & testpp_project_loader_i::settable_loader()
+{
+	static default_testpp_project_loader_c default_loader;
+	static testpp_project_loader_i * loader( &default_loader );
+	return loader;
+}
+
---
+++ testpp_project.h
@@ -0,0 +1,99 @@
+#ifndef TESTPP_PROJECT_H
+#define TESTPP_PROJECT_H
+
+#include <list>
+
+
+/**
+ * Declare a project to override various aspects of the tests.
+ * This is the principle manner in which to get a project loaded.
+ */
+#define TESTPP_PROJECT() \
+	class override_testpp_project_c : public testpp_project_c { \
+		virtual void init(); \
+	}; \
+	testpp_project_loader_c< override_testpp_project_c > project_loader; \
+	void override_testpp_project_c::init()
+
+
+
+class testpp_output_i;
+
+/**
+ * The default project for testpp.  The init() function can be overridden
+ * to customize the unit tests.
+ */
+class testpp_project_c
+{
+public:
+	/**
+	 * Construct the base project
+	 */
+	testpp_project_c();
+	/**
+	 * Destroy the project.
+	 */
+	~testpp_project_c();
+
+	virtual void init() {}
+
+protected:
+	template < class T >
+	void add_output()
+	{
+		m_output.push_back( new T() );
+	}
+
+private:
+	std::list< testpp_output_i * > m_output;
+};
+
+
+/**
+ * Project loader base class
+ */
+class testpp_project_loader_i
+{
+public:
+	/**
+	 * Create the project associated with this loader.
+	 */
+	virtual testpp_project_c * create_project() = 0;
+
+	/**
+	 * Provide the project loader to use for these tests.
+	 * This may be set with the testpp_project_loader_c class
+	 * or preferably, with the TESTPP_PROJECT macro.
+	 */
+	static testpp_project_loader_i & project()
+	{
+		return *settable_project();
+	}
+
+protected:
+	/**
+	 * Static function for clarifying initialization order.
+	 */
+	static testpp_project_loader_i * & settable_project_loader();
+};
+
+/**
+ * Templated project loader.  Declaring one of these for a specific
+ * project type will set that project as the project to use for all tests.
+ * In most cases, this class won't be used directly, but through the
+ * TESTPP_PROJECT macro. 
+ */
+template < class T >
+class testpp_project_loader_c
+{
+public:
+	testpp_project_loader_c()
+	{
+		settable_project() = this;
+	}
+	virtual testpp_project_c * create_project() { return new T(); }
+};
+
+
+#endif
+
