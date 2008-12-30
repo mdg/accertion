33d1aa6278c284a407ccd896db2e6f6dc51d01e9
Matthew Graham <mdg149@optonline.net>
2008-12-29 21:07:01 -0500

    minor rearrangement of whitespace in test files.
    tried to separate the setup code from the tested code.

--- test/default_output_test.cpp
+++ test/default_output_test.cpp
@@ -10,8 +10,8 @@ TESTPP( test_human_summary )
 	std::ostringstream out;
 	human_testpp_output_c output;
 	output.set_stream( out );
-	output.summarize( 8, 2, 1, 3 );
 
+	output.summarize( 8, 2, 1, 3 );
 	assertpp( out.str() ) == "2 failures in 10 tests\n"
 		"1 ignored and 3 not implemented\n";
 }
@@ -25,8 +25,8 @@ TESTPP( test_yaml_summary )
 	std::ostringstream out;
 	yaml_testpp_output_c output;
 	output.set_stream( out );
-	output.summarize( 8, 2, 1, 3 );
 
+	output.summarize( 8, 2, 1, 3 );
 	assertpp( out.str() ) ==
 		"passed: 8\n"
 		"failed: 2\n"
