2a08bfa249de8dd1347418c322017b7635159089
Matthew Graham <mdg149@gmail.com>
2012-02-21 19:29:34 -0500

    add accert run script
    this script runs an accertion test program. the run happens outside
    of the binary to be able to recover from segfaults and such

--- accertion.cc
+++ accertion.cc
@@ -117,11 +117,6 @@ int accertion_main(int argc, const char **argv)
 		print_tests(cout);
 	} else if (argc == 2) {
 		bool success(run_test(argv[1]));
-		if (success) {
-			cout << "pass\n";
-		} else {
-			cout << "fail\n";
-		}
 		result = success ? 0 : 1;
 	} else {
 		cerr << "missing test\n";
